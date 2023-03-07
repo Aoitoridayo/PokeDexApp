85aaadbe1f9e759bf40cdf8949a3b019f31a9f07
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-03-07 15:52:39 +0900

    Initial Commit

---
+++ PokeDexApp/PokeDexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexAppApp.swift
+//  PokeDexApp
+//
+//  Created by 渡邊魁優 on 2023/03/07.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
